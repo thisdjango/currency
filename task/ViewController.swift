0bab3b4eea04ac924b1fc20a7ead1e8f712163d4
AMCAdmin <amcadmin@admins-mbp-51.lan>
2019-08-26 15:09:45 +0300

    Initial Commit

---
+++ task/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  task
+//
+//  Created by AMCAdmin on 26/08/2019.
+//  Copyright © 2019 thisdjango. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
