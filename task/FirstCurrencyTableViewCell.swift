dee52e3963d66cfd57b469cd67d35147c20d3e35
AMCAdmin <amcadmin@admins-mbp-51.lan>
2019-08-26 19:27:30 +0300

    interface

---
+++ task/FirstCurrencyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FirstCurrencyTableViewCell.swift
+//  task
+//
+//  Created by AMCAdmin on 26/08/2019.
+//  Copyright © 2019 thisdjango. All rights reserved.
+//
+
+import UIKit
+
+class FirstCurrencyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
